67b33a35ebadd18cdfd7877b94d79dc6370bef13
mshaik791 <mohammedhnshaik@gmail.com>
2024-02-05 15:42:34 -0800

    complete

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+import "../styles/base.scss";
+import "../styles/loader.scss";
+import "../styles/landing.scss";
+import "../styles/coolLinks.scss";
+import "../styles/skills.scss";
+import "../styles/projects.scss";
+import "../styles/about.scss";
+import "../styles/footer.scss";
+
+
---
+++ assets/js/images.js
@@ -0,0 +1,30 @@
+import bg1 from '../images/loaders/shapes/19.jpg';
+import bg2 from '../images/loaders/shapes/24.jpg';
+import bg3 from '../images/loaders/shapes/26.jpg';
+import bg4 from '../images/loaders/shapes/44.jpg';
+import bg5 from '../images/loaders/shapes/213.png';
+import bg6 from '../images/loaders/shapes/220.jpg';
+import me1 from '../images/pics/pic1.jpg';
+import me2 from '../images/pics/pic2.jpg';
+import me3 from '../images/pics/pic3.png';
+import me4 from '../images/pics/pic5.png';
+import me5 from '../images/pics/pic4.png';
+import me6 from '../images/pics/pic6.png';
+
+
+
+const images = {
+    bg1,
+    bg2,
+    bg3, 
+    bg4, 
+    bg5,
+    bg6,
+    me1,
+    me2,
+    me3, 
+    me4, 
+    me5, 
+    me6,
+}
+export default images;
