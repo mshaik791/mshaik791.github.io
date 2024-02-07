8cbcfcb9269cf97b87524bf3c6b4e4a3f561ea63
mshaik791 <mohammedhnshaik@gmail.com>
2024-02-07 15:26:45 -0800

    updates

--- assets/js/images.js
+++ assets/js/images.js
@@ -4,12 +4,12 @@ import bg3 from '../images/loaders/shapes/26.jpg';
 import bg4 from '../images/loaders/shapes/44.jpg';
 import bg5 from '../images/loaders/shapes/213.png';
 import bg6 from '../images/loaders/shapes/220.jpg';
-import me1 from '../images/pics/pic1.jpg';
+import me1 from '../images/pics/pic3.png';
 import me2 from '../images/pics/pic2.jpg';
-import me3 from '../images/pics/pic3.png';
-import me4 from '../images/pics/pic5.png';
+import me3 from '../images/pics/pic6.jpg';
+import me4 from '../images/pics/pic1.jpg';
 import me5 from '../images/pics/pic4.png';
-import me6 from '../images/pics/pic6.png';
+import me6 from '../images/pics/pic6.jpg';
 
 
 
